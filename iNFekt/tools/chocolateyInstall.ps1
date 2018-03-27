5c2676e0750819ac53ef9fefd2c62c2ce479505e
mica <mica@protonmail.com>
2018-03-26 21:30:03 -0700

    1.0.1

---
+++ iNFekt/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+Install-ChocolateyPackage `
+    'infekt' `
+    'exe' `
+    '/verysilent /norestart' `
+    'https://syndicode.org/infekt/downloads/v1.0.1/iNFekt-v1.0.1-setup.exe' `
+    -checksum 'CFE3B5639BAAE17301645E946386FFCEBD3C246CA164E8999409082C0107531D' `
+    -checksumType 'sha256'
