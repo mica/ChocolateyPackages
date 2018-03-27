dc314b0073be3a2dd35d088b6e93dae5853c4de4
mica <mica@protonmail.com>
2018-03-26 22:14:19 -0700

    ToneLib 1.1.518

---
+++ ToneLib/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+Install-ChocolateyPackage `
+    'tonelib' `
+    'msi' `
+    '/quiet' `
+    'http://www.tonelib.net/download/Tonelib_Win.msi' `
+    -checksum 'CCD1999117BA7D94DCAFA8E85A0BE8BE4E916683471F69BC3F99C63056970E14' `
+    -checksumType 'sha256'
