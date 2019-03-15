3ec354e1833c6f39171455b964382b9878986d2f
mica <mica@pm.me>
2019-03-15 11:07:56 -0700

    Partition Wizard 11.0.1

---
+++ PartitionWizard/tools/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+Install-ChocolateyPackage `
+    -PackageName 'partitionwizard' `
+    -SilentArgs '/VERYSILENT' `
+    -Url 'https://www.partitionwizard.com/download/v11/pw11-free.exe' `
+    -Checksum '80C15EC8AF32600F7B434DAA948D2B3A84B636FF93ED02F377111BBEAAB887DB' `
+    -ChecksumType 'sha256'
