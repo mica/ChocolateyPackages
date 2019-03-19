553ea311d551d4c8c200a1776b3b979d3d21bd53
mica <mica@pm.me>
2019-03-19 12:17:58 -0700

    Breevy 3.37

---
+++ Breevy/tools/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+Install-ChocolateyPackage `
+    -PackageName 'breevy' `
+    -SilentArgs '/S' `
+    -Url 'http://www.16software.com/breevy/Breevy_Setup.exe' `
+    -Checksum 'C7F016BD443D596C4E15FD8D52760EC1141E7B3A3FEB1BC1BDFCCD12549E19B8' `
+    -ChecksumType 'sha256'
