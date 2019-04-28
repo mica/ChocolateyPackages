fe0fead04473ded53ff77cc1e35aa0c0ee7d19fd
mica <mica@pm.me>
2019-04-27 22:10:29 -0700

    SciTE4AutoIt3 19.102.1901.001

--- SciTE4AutoIt3/tools/chocolateyInstall.ps1
+++ SciTE4AutoIt3/tools/chocolateyInstall.ps1
@@ -2,5 +2,5 @@ Install-ChocolateyPackage `
     -PackageName 'SciTE4AutoIt3' `
     -SilentArgs '/S' `
     -Url 'https://www.autoitscript.com/autoit3/scite/download/SciTE4AutoIt3.exe' `
-    -Checksum '7C14C2499E8849EB9FC0EBC544EE417AD3A17D83E2B51EC9B767B3F40BDF013B' `
+    -Checksum '7A8E6D23C225DC848CDAADD9DB13CE4C90F4DF7B3A903227C9F491CF857C4AE2' `
     -ChecksumType 'sha256'
