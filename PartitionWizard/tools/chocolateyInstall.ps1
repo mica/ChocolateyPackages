76af435927c34a3309f3745b6cfb6fb5ac33f3ce
mica <mica@users.noreply.github.com>
2019-06-01 11:01:27 -0700

    Partition Wizard 11.4

--- PartitionWizard/tools/chocolateyInstall.ps1
+++ PartitionWizard/tools/chocolateyInstall.ps1
@@ -2,5 +2,5 @@ Install-ChocolateyPackage `
     -PackageName 'partitionwizard' `
     -SilentArgs '/VERYSILENT' `
     -Url 'https://www.partitionwizard.com/download/v11/pw11-free.exe' `
-    -Checksum '80C15EC8AF32600F7B434DAA948D2B3A84B636FF93ED02F377111BBEAAB887DB' `
+    -Checksum '4FB0F82503F0CC084812E20EFDDA856D6FFA6AF69AB581DC3199C5403366B324' `
     -ChecksumType 'sha256'
