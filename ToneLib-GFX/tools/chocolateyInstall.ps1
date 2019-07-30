31e5254abbe4dd5ed8b40b9f02ef6dbe933b48c0
mica <mica@users.noreply.github.com>
2019-07-30 12:46:18 -0700

    ToneLib GFX 3.9.7

--- ToneLib-GFX/tools/chocolateyInstall.ps1
+++ ToneLib-GFX/tools/chocolateyInstall.ps1
@@ -3,5 +3,5 @@ Install-ChocolateyPackage `
     -FileType 'exe' `
     -SilentArgs '/silent' `
     -Url 'https://www.tonelib.net/download/ToneLib-GFX-Win64.exe' `
-    -checksum 'B70DE4226B65D914478E0D198B3A13B135D173B01F83379FB777013BD5757473' `
+    -checksum '86E94B73859FB1BD0E3AAB739AA65F62037EFF78C85EF625AC4BB3B5FF451D0F' `
     -checksumType 'sha256'
