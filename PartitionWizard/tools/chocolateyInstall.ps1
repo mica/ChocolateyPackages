69c69b60f7bd850bcb1167c620e51525f9433942
mica <mica@pm.me>
2019-11-24 22:06:44 -0800

    Checksum update

--- PartitionWizard/tools/chocolateyInstall.ps1
+++ PartitionWizard/tools/chocolateyInstall.ps1
@@ -2,5 +2,5 @@ Install-ChocolateyPackage `
     -PackageName 'partitionwizard' `
     -SilentArgs '/VERYSILENT' `
     -Url 'https://www.partitionwizard.com/download/v11/pw11-free.exe' `
-    -Checksum '1DB3190851B29AEA98BCD8C70EA24B22BBA2B64295FAFB58E5DB23540C9B6F92' `
+    -Checksum '865B9EA053915779C1CAD37D1B33B89920A2F06EAC273303A455C91F7D2BE423' `
     -ChecksumType 'sha256'
