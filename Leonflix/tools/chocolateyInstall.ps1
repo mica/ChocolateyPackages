ba090b3934afce37a4364c3684ecbcdddf95bfd1
mica <mica@users.noreply.github.com>
2019-12-11 00:16:58 -0800

    0.7.0

---
+++ Leonflix/tools/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+Install-ChocolateyPackage `
+    -PackageName 'leonflix' `
+    -SilentArgs '/S' `
+    -Url 'https://leonflix.net/downloads/Leonflix-Installer.exe' `
+    -Checksum 'FAEBB762F63BE8D5F6FA42C8A2B13F3492F2465A9403FC4417B7ECF5CEB7407F' `
+    -ChecksumType 'sha256'
