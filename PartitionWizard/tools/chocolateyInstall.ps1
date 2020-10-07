372f5df54fdb5f29b8b5f8e09904b3425b01aa8b
mica <mica@pm.me>
2020-10-06 17:27:01 -0700

    12.0

--- PartitionWizard/tools/chocolateyInstall.ps1
+++ PartitionWizard/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 Install-ChocolateyPackage `
     -PackageName 'partitionwizard' `
     -SilentArgs '/VERYSILENT' `
-    -Url 'https://cdn2.minitool.com/download-center/release/products/pw/v12/pw12-free-offline.exe' `
-    -Checksum 'CD4142F00D8AD88297324CF3A4F63BB0C114296C03F5ABF810FE92A3F10C9141' `
+    -Url 'https://cdn2.minitool.com/?p=pw&e=pw-free-offline' `
+    -Checksum 'D741D6B4C2570D9975B6E58517220A7B60B3C720DDA53F71D123DF1CE80EAD2B' `
     -ChecksumType 'sha256'
