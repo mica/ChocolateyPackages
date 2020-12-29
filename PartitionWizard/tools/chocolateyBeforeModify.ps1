503f7b286bbfab35de6041d111db820df1ceebb2
mica <mica@pm.me>
2020-12-28 22:32:47 -0800

    12.3

--- PartitionWizard/tools/chocolateyBeforeModify.ps1
+++ PartitionWizard/tools/chocolateyBeforeModify.ps1
@@ -1,2 +1,8 @@
-$ErrorActionPreference = 'SilentlyContinue'
-Start-Process "$env:ProgramFiles\MiniTool Partition Wizard 11\unins000.exe" -ArgumentList '/VERYSILENT' -Wait
+$11 = "$env:ProgramFiles\MiniTool Partition Wizard 11"
+$12 = "$env:ProgramFiles\MiniTool Partition Wizard 12"
+
+if (Test-Path $11) {
+    Start-Process (Get-ChildItem $11\unins*.exe).FullName -ArgumentList '/VERYSILENT' -Wait
+} elseif (Test-Path $12) {
+    Start-Process (Get-ChildItem $12\unins*.exe).FullName -ArgumentList '/VERYSILENT' -Wait
+}
--- PartitionWizard/tools/chocolateyInstall.ps1
+++ PartitionWizard/tools/chocolateyInstall.ps1
@@ -2,5 +2,5 @@ Install-ChocolateyPackage `
     -PackageName 'partitionwizard' `
     -SilentArgs '/VERYSILENT' `
     -Url 'https://cdn2.minitool.com/?p=pw&e=pw-free-offline' `
-    -Checksum 'D741D6B4C2570D9975B6E58517220A7B60B3C720DDA53F71D123DF1CE80EAD2B' `
+    -Checksum '4D5D2FAE722A85B787170CCFDC224B57E40EEF65AD31EF933554D725F1C43E41' `
     -ChecksumType 'sha256'
