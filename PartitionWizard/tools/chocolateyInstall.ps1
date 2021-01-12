6cfb194937fd0930195871d2b5d583d6435fd4cc
mica <mica@pm.me>
2021-01-11 23:23:56 -0800

    12.3

--- PartitionWizard/tools/chocolateyInstall.ps1
+++ PartitionWizard/tools/chocolateyInstall.ps1
@@ -2,5 +2,5 @@ Install-ChocolateyPackage `
     -PackageName 'partitionwizard' `
     -SilentArgs '/VERYSILENT' `
     -Url 'https://cdn2.minitool.com/?p=pw&e=pw-free-offline' `
-    -Checksum '4D5D2FAE722A85B787170CCFDC224B57E40EEF65AD31EF933554D725F1C43E41' `
+    -Checksum 'D81D7010613221B01F444BB60E037E5753866BECCFAEA6ACF3A07B08E0B6C584' `
     -ChecksumType 'sha256'
