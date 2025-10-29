4fb90efc29d5d951afae65cfe29db635cecb41f8
mica <mica@users.noreply.github.com>
2025-10-29 14:00:56 -0700

    .

---
+++ VolumeScroller/tools/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+Install-ChocolateyPackage `
+    -PackageName 'volumescroller' `
+    -SilentArgs '/VERYSILENT' `
+    -Url 'https://github.com/patrickiel/VolumeScroller/releases/download/2.2.2/Volume.Scroller.2.2.2.exe' `
+    -Checksum 'B2593C79B567B2ED6C2A69CA70D32C3DBACAE291D6F0EED3A540EED2EBD222A1' `
+    -ChecksumType 'sha256'
